4d30812fd58a1403e50bb709ec69c407d57f8597
Malvin Alexious <c14220245@john.petra.ac.id>
2024-10-16 00:01:43 +0700

    init:total_progress

---
+++ templates/template_page_legacy.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.markdown(
+    """
+    <style>
+        [data-testid="stSidebarNavItems"] {
+            display: none;
+        }
+    </style>
+    """, unsafe_allow_html=True
+)
+
+st.title("Template")
+st.write("Ini template")
+
+# Riwayat Percakapan
